2fed6298960aace0c091b5f4f2f9a8c5c238a065
Lenny Zeitoun <zeitounlenny@gmail.com>
2021-01-10 18:46:31 +0200

    Changed back to original

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "load_balancer",
     script: "./server_lb.js",
-    watch: ["./.authorized_servers.json", "./.env"],
+    watch: ["./.pool_of_servers.json", "./.authorized_servers.json", "./.env"],
     // Delay between restart
     autorestart: true,
     watch_delay: 100,
